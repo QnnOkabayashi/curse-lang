ab58d5a80e470aee2cee17194e521b1c6b777348
William Ball <williampi103@gmail.com>
2023-03-02 22:52:56 -0500

    fixed shadowing bug

--- src/interpreter/builtins.rs
+++ src/interpreter/builtins.rs
@@ -42,17 +42,16 @@ pub fn call_function<'ast, 'input>(
         Value::Builtin(f) => f(left, right, env),
         Value::Closure(ast::Closure { params, body }) => match params {
             Params::Two(Pat::Item(x), Pat::Item(y)) => {
-                env.insert(x.inner, left);
-                env.insert(y.inner, right);
-                let result = eval(body, env)?;
-                env.remove(x.inner);
-                env.remove(y.inner);
+                let mut new_env = env.clone();
+                new_env.insert(x.inner, left);
+                new_env.insert(y.inner, right);
+                let result = eval(body, &mut new_env)?;
                 Ok(result)
             }
             Params::One(Pat::Item(x)) => {
-                env.insert(x.inner, left);
-                let result = eval(body, env)?;
-                env.remove(x.inner);
+                let mut new_env = env.clone();
+                new_env.insert(x.inner, left);
+                let result = eval(body, &mut new_env)?;
                 Ok(result)
             }
             _ => todo!(),
